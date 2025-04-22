2ea1f09460e5243be8e41748415daac82b87a8e2
Roaa-S09 <rs2095@scarletmail.rutgers.edu>
2025-04-21 21:37:46 -0400

    Moved loginPage.html to templates folder and updated app.py routes

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+import os
+
+app = Flask(__name__, template_folder='Login/templates')
+
+def db_connect():
+    conn = sqlite3.connect("Login/studentAcc.db")
+    conn.row_factory = sqlite3.Row
+    return conn
+
+@app.route("/", methods = ["GET", "POST"])
+def login():
+    error = None
+    if request.method == "POST":
+        netid = request.form["netid"]
+        password = request.form["password"]
+
+        conn = db_connect()
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM student_info WHERE netid = ? AND password = ?", (netid, password))
+        user = cursor.fetchone()
+        conn.close()
+
+        if user:
+            return redirect(url_for("home", netid = netid))
+        else:
+            error = "Invalid NetID or Password"
+
+    return render_template("loginPage.html", error=error)
+    
+@app.route("/home")
+def home():
+    return render_template("homePage.html")
+    
+if __name__== "__main__":
+    app.run(debug=True)
