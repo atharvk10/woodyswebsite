be6bd452d0125a8317f312c031e3436ce181ad25
Roaa-S09 <rs2095@scarletmail.rutgers.edu>
2025-04-22 16:53:29 -0400

    Created login.js, users.js, and studentAccPage.js.updated html files as well as css files.

---
+++ Login/login.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('loginForm');
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const netidInput = document.getElementById('netid').value.trim();
+        const passwordInput = document.getElementById('password').value.trim();
+
+        const user = users.find(
+            (u) => u.netid === netidInput && u.password === passwordInput
+        );
+
+        if (user) {
+            localStorage.setItem("netid", user.netid);
+            localStorage.setItem("num_meal_swipes", user.num_meal_swipes);
+            localStorage.setItem("retail_swipes", user.retail_swipes);
+            
+            window.location.href = 'studentAccPage.html';
+        }else{
+            alert('Invalid NetID or Password. Please try again');
+        }
+    });
+});
