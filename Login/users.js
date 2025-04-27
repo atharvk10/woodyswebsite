a61eb7b9fb7dac5fdc2172a8f99103c88946af21
marygrlic <marygrlic@gmail.com>
2025-04-27 17:17:46 -0400

    edits to menu to direct to student account

--- Login/users.js
+++ Login/users.js
@@ -2,15 +2,17 @@
 
 const users = [
     {
-        netid: "John_doe", 
+        netid: "jd123", 
         password: "password123",
         num_meal_swipes: 5,
-        retail_swipes: 3
+        retail_swipes: 3,
+        reward_points: 0
     },
     {
-        netid: "Jane_doe", 
+        netid: "jed0190", 
         password: "password456",
         num_meal_swipes: 2,
-        retail_swipes: 1
+        retail_swipes: 1,
+        reward_points: 100
     }
 ];
